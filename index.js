1caa407982fcf6d19c49150c82bd8b1d45700cc1
dpizzle38 <130174020+dpizzle38@users.noreply.github.com>
2023-06-30 04:21:36 -0700

    submitting

--- index.js
+++ index.js
@@ -1,86 +1,80 @@
-const firstPara = document.querySelector("p");
-console.log(firstPara);
+// Question 1
 
-const firstParaID = document.querySelector("firstP");
-const secondParaID = document.querySelector("secondP");
-const thirdParaID = document.querySelector("thirdP");
-const fourthParaID = document.querySelector("fourthP");
+const para = document.querySelector("p");
+console.log(para);
+
+
+// Question 2
+const firstParaID = document.querySelector("#firstP");
+const secondParaID = document.querySelector("#secondP");
+const thirdParaID = document.querySelector("#thirdP");
+const fourthParaID = document.querySelector("#fourthP");
 
 console.log(firstParaID);
 console.log(secondParaID);
 console.log(thirdParaID);
 console.log(fourthParaID);
 
+// Question 3
+
 const allPara = document.querySelectorAll("p");
-console.log(allPara);
 
 for(let i = 0; i < allPara.length; i++){
-    console.log(allPara[i].textContent);
-}
+    allPara[0].textContent = "Winners never quit";
+    allPara[1].textContent = "Quitters never win";
+    allPara[2].textContent = "Givers never lack";
+    allPara[3].textContent = "Fourth Paragraph";
+    
+    allPara[0].setAttribute("id", "firstP");
+    allPara[0].setAttribute("class", "para1");
+    allPara[1].setAttribute("id", "secondP");
+    allPara[1].setAttribute("class", "para2" );
+    allPara[2].setAttribute("id", "thirdP");
+    allPara[2].setAttribute("class", "para3");
+    allPara[3].setAttribute("id", "fourthP");
+    allPara[3].setAttribute("class", "para4");
 
+    // Exercise : Level 2
 
-firstParaID[0].id = "firstP";
-firstParaID[0].className = "para1";
-firstParaID[0].setAttribute("id", "firstP");
-firstParaID[0].setAttribute ("class", "para1");
-secondParaID[1].id = "secondP";
-secondParaID[1].className = "para2";
-secondParaID[1].setAttribute ("id", "secondP");
-secondParaID[1].setAttribute("class", "para2");
-thirdParaID[2].id = "thirdP";
-thirdParaID[2].className = "para3";
-thirdParaID[2].setAttribute("id", "thirdP");
-thirdParaID[2].setAttribute("class", "para3");
-fourthParaID[3].id = "fourthP"
-fourthParaID[3].className = "para4";
-fourthParaID[3].setAttribute("id", "fourthP");
-fourthParaID[3].setAttribute("class", "para4");
+    // 1
+    allPara[0].style.color = "green";
+    allPara[0].style.backgroundColor = "blue"
+    allPara[0].style.border = "black"
+    allPara[0].style.fontSize = "25px";
+    allPara[0].style.fontFamily = "poppins";
 
+    allPara[1].style.color = "black";
+    allPara[1].style.backgroundColor = "orange";
+    allPara[1].style.border = "grey";
+    allPara[1].style.fontSize = "15px";
+    allPara[1].style.fontfamily = "poppins";
 
-firstParaID.style.color = "red";
-secondParaID.style.color = "blue";
-thirdParaID.style.color = "green";
-fourthParaID.style.colr = "black"
+    allPara[2].style.color = "green";
+    allPara[2].style.backgroundColor = "blue";
+    allPara[2].style.border = "black";
+    allPara[2].style.fontSize = "15px";
+    allPara[2].style.fontFamily = "poppins";
 
-firbstParaID.style.background = "black";
-secondParaID.style.background = "red";
-thirdParaID.style.background = "green";
-fourthParaID.style.background = "blue";
-
-firstParaID.style.border = "solid1px blue";
-secondParaID.style.border = "solid1px red";
-thirdParaID.style.border = "solid1px black";
-fourthParaID.style.border = "solid1px grey";
-
-
-firstParaID.style.fontsize = "25px";
-secondParaID.style.fontsize = "15px";
-thirdParaID.style.fontsize = "18px";
-fourthParaID.style.fontsize = "20px";
-
-firstParaID.style.fontfamily = "poppins";
-secondParaID.style.fontfamily = "poppins";
-thirdParaID.style.fontfamily = "poppins";
-fourthParaID.style.fontfamily = "Arial";
-
-firstParaID.style.color = "green";
-thirdParaID.style.color = "green";
-
-secondParaID.style.color = "red";
-fourthParaID.style.color = "red";
-
-for(let i = 0; i < allPara.length; i++){
-    firstParaID.style.color = "green";
-    thirdParaID.style.color = "green";
-    secondParaID.style.color = "red";
-    fourthParaID.style.color = "red";
+    allPara[3].style.color = "white";
+    allPara[3].style.background = "black";
+    allPara[3].style.border = "grey";
+    allPara[3].style.fontSize = "15px";
+    allPara[3].style.fontFamily = "poppins";
     console.log(allPara[i]);
 }
 
-for(let i = 0; i < allPara.length; i++){
-    firstParaID.setAttribute.id = "firstP";
-    secondParaID.setAttribute.id = "secondP";
-    thirdParaID.setAttribute.id = "thirdP";
-    fourthParaID.setAttribute.id = "fourthP";
-    console.log(allPara[i]);
-}
+// 2
+
+const allParaID = document.querySelectorAll("p");
+
+allParaID.forEach(allPar => {
+   if(1 % 2 === 0 ){
+    allPar.style.color = "red";
+   } else {
+    allPar.style.color = "green";
+   }
+
+   
+
+});
+
